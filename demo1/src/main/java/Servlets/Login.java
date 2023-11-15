9873db17c46c4f844ef4d068f15c713ae17340e1
ole <andreoverpowered@gmail.com>
2023-11-15 17:36:21 +0100

    push

--- demo1/src/main/java/Servlets/Login.java
+++ demo1/src/main/java/Servlets/Login.java
@@ -1,16 +1,23 @@
 package Servlets;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 @WebServlet(name = "servlet",urlPatterns = {"/Home"})
 
 public class Login extends HttpServlet{
-        public void checkLogin(HttpServletRequest req, HttpServletResponse res){
-
+        public void doGet(HttpServletRequest req, HttpServletResponse res)throws ServletException, IOException {
+                res.setContentType("text/html;charset=UTF-8");
+                try (PrintWriter out = res.getWriter()) {
+                        String username = req.getParameter("username");
+                        String password = req.getParameter("password");
+                }
         }
 }
 
