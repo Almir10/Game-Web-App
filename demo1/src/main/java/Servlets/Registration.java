8d02edba46c34e64edc34d9397a783d68d5e63f4
Almir10 <karahodzaalmir4@gmail.com>
2023-11-15 19:07:22 +0100

    Lagano

--- demo1/src/main/java/Servlets/Registration.java
+++ demo1/src/main/java/Servlets/Registration.java
@@ -14,5 +14,52 @@ import Beans.Users;
 
 public class Registration extends HttpServlet{
 
-}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            // Preuzimanje parametara iz zahteva
+            String email = request.getParameter("email");
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+
+            // Povezivanje na bazu podataka
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ucenje", "root", "almir12345")) {
+                // Provera da li korisnik već postoji u bazi podataka
+                String checkUserQuery = "SELECT * FROM tabela WHERE username=?";
+                try (PreparedStatement checkUserStatement = connection.prepareStatement(checkUserQuery)) {
+                    checkUserStatement.setString(1, username);
+
+                    try (var result = checkUserStatement.executeQuery()) {
+                        if (result.next()) {
+                            // Korisnik već postoji, ispišite poruku
+                            out.println("Korisničko ime već postoji. Molimo vas da izaberete drugo korisničko ime.");
+                        } else {
+                            // Ako korisnik ne postoji, dodajte ga u bazu podataka
+                            String insertUserQuery = "INSERT INTO tabela (email, username, password) VALUES (?, ?, ?)";
+                            try (PreparedStatement insertUserStatement = connection.prepareStatement(insertUserQuery)) {
+                                insertUserStatement.setString(1, email);
+                                insertUserStatement.setString(2, username);
+                                insertUserStatement.setString(3, password);
+
+                                // Izvršavanje upita za dodavanje korisnika
+                                int rowsAffected = insertUserStatement.executeUpdate();
+
+                                if (rowsAffected > 0) {
+                                    // Korisnik je uspešno registrovan
+                                    out.println("Uspešna registracija! Sada se možete prijaviti.");
+                                } else {
+                                    // Nešto je pošlo po zlu pri dodavanju korisnika
+                                    out.println("Došlo je do greške prilikom registracije. Molimo vas pokušajte ponovo.");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database:");
+            e.printStackTrace();
+        }
+
+    }}
 
